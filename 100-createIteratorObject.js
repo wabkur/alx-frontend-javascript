c6b14f91b6156f669d6de2f45f6084eb93fdf973
wabkur <wabilitukur.com>
2024-10-03 22:09:45 +0300

    basic

---
+++ 100-createIteratorObject.js
@@ -0,0 +1,9 @@
+port default function createIteratorObject(report) {
+  return (function* _() {
+    for (const department of Object.values(report.allEmployees)) {
+      for (const employee of department) {
+        yield employee;
+      }
+    }
+  }());
+}
