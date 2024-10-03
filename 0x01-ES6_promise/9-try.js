e061df21754bc26e19fc4342716d78499eff96d9
wabkur <wabilitukur.com>
2024-10-03 23:07:28 +0300

    basic

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,13 +1,11 @@
 export default function guardrail(mathFunction) {
-    const queue = [];
-  
-    try {
-      queue.push(mathFunction());
-    } catch (err) {
-      queue.push(String(err));
-    } finally {
-      queue.push('Guardrail was processed');
-    }
-  
-    return queue;
+  const queue = [];  
+  try {
+    queue.push(mathFunction());
+  } catch (err) {
+    queue.push(String(err));
+  } finally {
+    queue.push('Guardrail was processed');
   }
+  return queue;
+}
