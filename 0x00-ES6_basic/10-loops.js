28693a08b32ad28c246fb797c84ca744c36117d8
wabkur <wabilitukur.com>
2024-10-03 21:46:03 +0300

    basic

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -2,6 +2,6 @@ export default function appendToEachArrayValue(array, appendString) {
   const newArray = [];
   for (const value of array) {
     newArray.push(appendString + value);
-  }  
+  }
   return newArray;
 }
--- 0x00-ES6_basic/7-getBudgetObject.js
+++ 0x00-ES6_basic/7-getBudgetObject.js
@@ -3,6 +3,6 @@ export default function getBudgetObject(income, gdp, capita) {
     income,
     gdp,
     capita,
-  };  
+  };
   return budget;
 }
