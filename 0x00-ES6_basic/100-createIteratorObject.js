3a8e44cd1e957ce71086860ebc0832a3abc4f8c8
wabkur <wabilitukur.com>
2024-10-03 22:20:49 +0300

    basic

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,5 +1,5 @@
-port default function createIteratorObject(report) {
-  return (function* _() {
+export default function createIteratorObject(report) {
+  return (function* () {
     for (const department of Object.values(report.allEmployees)) {
       for (const employee of department) {
         yield employee;
